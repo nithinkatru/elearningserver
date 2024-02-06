cfecd3d15ec8dd2e8def37d1ccbff56eaff9e1bf
nithi <nithinkumarkatru@gmail.com>
2024-02-05 21:00:28 -0500

    this is the server side of  complete signup and login page fully functional

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const bcrypt = require('bcryptjs');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Connect to MongoDB
 mongoose.connect('mongodb+srv://nithinkumarkatru:hello@cluster0.zrplywq.mongodb.net/');
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
@@ -13,30 +15,65 @@ db.once('open', () => {
   console.log('Connected to MongoDB');
 });
 
+// User schema
 const UserSchema = new mongoose.Schema({
   firstName: String,
   lastName: String,
-  email: String,
+  email: { type: String, unique: true }, // Ensure email is unique
   phoneNumber: String,
   role: String,
-  password: String // Consider hashing this before storage
- 
+  password: String
 });
-console.log('your in user schema')
+
 const User = mongoose.model('User', UserSchema);
 
 app.use(bodyParser.json());
 app.use(cors());
 
+// Endpoint to create a new user with hashed password
 app.post('/api/users', async (req, res) => {
   const { firstName, lastName, email, phoneNumber, role, password } = req.body;
-  // Add server-side validation here as needed
-  const newUser = new User({ firstName, lastName, email, phoneNumber, role, password });
+
+  // Hash password before saving
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const newUser = new User({ firstName, lastName, email, phoneNumber, role, password: hashedPassword });
+
   try {
     await newUser.save();
     res.status(201).send('User created successfully');
   } catch (error) {
+    console.error(error);
     res.status(500).send('Error creating user');
+  }
+});
+
+
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/api/login', async (req, res) => {
+ const { email } = req.body;
+  console.log(email)
+  // console.log(password)
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).send('Invalid email ');
+    }
+
+    // const isMatch = await bcrypt.compare(password, user.password);
+    // if (!isMatch) {
+    //   return res.status(400).send('Invalid email or password.');
+    // }
+
+    res.send('Login successful');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
   }
 });
 
