99c666ddb4fea4b80ff8cbcd03b857e9fa666d41
nithi <nithinkumarkatru@gmail.com>
2024-02-05 19:24:29 -0500

    the updated server code 3

--- server.js
+++ server.js
@@ -1,61 +1,45 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const md5 = require('md5');
-const cors = require('cors'); // Import the cors package
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Connect to MongoDB
-mongoose.connect('mongodb+srv://nithinkumarkatru:hello@cluster0.zrplywq.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://nithinkumarkatru:hello@cluster0.zrplywq.mongodb.net/');
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => {
   console.log('Connected to MongoDB');
 });
 
-// Define schema for user data
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   firstName: String,
   lastName: String,
   email: String,
   phoneNumber: String,
   role: String,
-  encryptedPassword: String
+  password: String // Consider hashing this before storage
+ 
 });
+console.log('your in user schema')
+const User = mongoose.model('User', UserSchema);
 
-const User = mongoose.model('User', userSchema);
-
-// Middleware
 app.use(bodyParser.json());
-app.use(cors()); // Use the cors middleware to allow cross-origin requests
+app.use(cors());
 
-// Routes
 app.post('/api/users', async (req, res) => {
+  const { firstName, lastName, email, phoneNumber, role, password } = req.body;
+  // Add server-side validation here as needed
+  const newUser = new User({ firstName, lastName, email, phoneNumber, role, password });
   try {
-    const { firstName, lastName, email, phoneNumber, role, password } = req.body;
-    const encryptedPassword = md5(password);
-    const newUser = new User({ firstName, lastName, email, phoneNumber, role, encryptedPassword });
     await newUser.save();
-    res.status(201).json(newUser);
+    res.status(201).send('User created successfully');
   } catch (error) {
-    console.error('Error saving user data:', error);
-    res.status(500).json({ error: 'Internal server error' });
+    res.status(500).send('Error creating user');
   }
 });
 
-app.get('/api/users', async (req, res) => {
-  try {
-    const users = await User.find();
-    res.json(users);
-  } catch (error) {
-    console.error('Error retrieving user data:', error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
-
-// Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
